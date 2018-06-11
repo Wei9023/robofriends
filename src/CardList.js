930e590e91c0338e7f31378e435598963d5f1950
Valensia Wei <valwwei@gmail.com>
2018-06-11 13:18:37 -0700

    CardList component

--- src/CardList.js
+++ src/CardList.js
@@ -2,15 +2,22 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({robots}) => {
-	const CardComponent = robots.map((user, i) => {
-		return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />
-	})
-
-	return (
-		<div>
-			{CardComponent}
-		</div>
-		)	
+		return (
+			<div>
+				{
+					robots.map((user, i) => {
+					return (
+							<Card 
+								key={i} 
+								id={robots[i].id} 
+								name={robots[i].name} 
+								email={robots[i].email} 
+							/>
+						);
+					})
+				}
+			</div>
+		);
 }
 
 export default CardList;
